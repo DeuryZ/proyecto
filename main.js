117c923fb860f4556303ca600e54f2128b6c469e
DeuryZ <119361476+DeuryZ@users.noreply.github.com>
2023-09-20 13:12:39 -0500

    feat: :lipstick:

--- main.js
+++ main.js
@@ -1,42 +1,19 @@
-/*let myForm=document.querySelector("form");
-let myTabla=document.querySelector("#myData");
-addEventListener("DOMContentLoaded", async()=>{
-    let res=await(await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla")).json();
-    for (let i = 0; i < res.length; i++) {
-        myTabla.insertAdjacentHTML("beforeend",`
-        <tr>
-            <td>${res[i].id}</td>
-            <td>${res[i].valor}</td>
-            <td>${res[i].caja}</td>
-        </tr>
-        `);
-    }
-})
-myForm.addEventListener("submit", async(e)=>{
-    e.preventDefault();
-    const data=Object.fromEntries(new FormData(e.target));
-    const {valor}=data;
-    data.valor=Number(valor);
-    let config={
-        method:"POST",
-        Headers:{"Content-Type":"application/json"},
-        body:JSON.stringify(data)
-    };
-    let res=await(await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla",config)).json();
-    console.log(res);
-})*/
-
 let myForm = document.querySelector("form");
 let myTabla = document.querySelector("#myData");
-let editar = document.se
+let myDelete = document.querySelector("#delete");
+let myUpdate = document.querySelector("#update");
+let myReset = document.querySelector("#reset");
+
 addEventListener("DOMContentLoaded", async () => {
     let res = await (await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla")).json();
     for (let i = 0; i < res.length; i++) {
         myTabla.insertAdjacentHTML("beforeend", `
         <tr>
             <td>${res[i].id}</td>
-            <td>${res[i].valor}</td>
-            <td>${res[i].caja}</td>
+            <td>${res[i].amount}</td>
+            <td>${res[i].type}</td>
+            <td><button id="delete">Delete</button></td>
+            <td><button id="update">Update</button></td>
         </tr>
         `);
     }
@@ -45,8 +22,8 @@ addEventListener("DOMContentLoaded", async () => {
 myForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     const data = Object.fromEntries(new FormData(e.target));
-    const { valor } = data;
-    data.valor = Number(valor);
+    const { amount } = data;
+    data.amount = Number(amount);
     let config = {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -56,3 +33,13 @@ myForm.addEventListener("submit", async (e) => {
     console.log(res);
 })
 
+
+
+myDelete.addEventListener("click", async (e) => {
+    let config = {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" }
+    };
+    let res = await (await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla", config)).json();
+    console.log(res);
+})
