8b933840c5c07b7db2dc22d09d5e36968a04fa68
DeuryZ <119361476+DeuryZ@users.noreply.github.com>
2023-09-21 13:13:39 -0500

    fix: :poop:

--- main.js
+++ main.js
@@ -2,6 +2,60 @@ let myForm = document.querySelector("form");
 let myTabla = document.querySelector("#myData");
 let myDelete = document.querySelector("#delete");
 let myUpdate = document.querySelector("#update");
+addEventListener("DOMContentLoaded", async () => {
+    let res = await (await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla")).json();
+    for (let i = 0; i < res.length; i++) {
+        myTabla.insertAdjacentHTML("beforeend", `
+        <tr>
+            <td>${res[i].id}</td>
+            <td>${res[i].amount}</td>
+            <td>${res[i].type}</td>
+            <td><button id="delete">Delete</button></td>
+            <td><button id="update">Update</button></td>
+        </tr>
+        `);
+    }
+})
+myForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.target));
+    const { amount } = data;
+    data.amount = Number(amount);
+    let config = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
+    };
+    let res = await (await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla", config)).json();
+    console.log(res);
+})
+
+myDelete.addEventListener("click", async (e) => {
+    e.preventDefault();
+    let config = {
+        method: "DELETE"
+    };
+    let eliminate ="https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla/"+5
+    let res = await (await fetch(eliminate, config)).json();
+    console.log(res);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let myForm = document.querySelector("form");
+let myTabla = document.querySelector("#myData");
+let myDelete = document.querySelector("#delete");
+let myUpdate = document.querySelector("#update");
 let myReset = document.querySelector("#reset");
 
 addEventListener("DOMContentLoaded", async () => {
@@ -42,4 +96,4 @@ myDelete.addEventListener("click", async (e) => {
     };
     let res = await (await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla", config)).json();
     console.log(res);
-})
+})*/ 
