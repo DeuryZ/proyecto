00d6af91756e14db5b6f1bf1df16d41636ac8beb
DeuryZ <119361476+DeuryZ@users.noreply.github.com>
2023-09-22 07:12:52 -0500

    feat: :poop:

--- main.js
+++ main.js
@@ -1,78 +1,59 @@
 let myForm = document.querySelector("form");
-let myTabla = document.querySelector("#myData");
-let myDelete = document.querySelector("#delete");
-let myUpdate = document.querySelector("#update");
-addEventListener("DOMContentLoaded", async () => {
-    let res = await (await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla")).json();
-    for (let i = 0; i < res.length; i++) {
-        myTabla.insertAdjacentHTML("beforeend", `
-        <tr>
-            <td>${res[i].id}</td>
-            <td>${res[i].amount}</td>
-            <td>${res[i].type}</td>
-            <td><button id="delete">Delete</button></td>
-            <td><button id="update">Update</button></td>
-        </tr>
-        `);
-    }
-})
-myForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const data = Object.fromEntries(new FormData(e.target));
-    const { amount } = data;
-    data.amount = Number(amount);
-    let config = {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data)
-    };
-    let res = await (await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla", config)).json();
-    console.log(res);
-})
+let myTable = document.querySelector("#myData");
 
-myDelete.addEventListener("click", async (e) => {
-    e.preventDefault();
-    let res = await (await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla")).json();
-    let config = {
-        method: "DELETE"
-    };
-    let eliminate ="https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla/"+res[0].id
-    res = await (await fetch(eliminate, config)).json();
-    console.log(res);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*let myForm = document.querySelector("form");
-let myTabla = document.querySelector("#myData");
-let myDelete = document.querySelector("#delete");
-let myUpdate = document.querySelector("#update");
-let myReset = document.querySelector("#reset");
 
 addEventListener("DOMContentLoaded", async () => {
-    let res = await (await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla")).json();
+    let res= await(await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla")).json();
     for (let i = 0; i < res.length; i++) {
-        myTabla.insertAdjacentHTML("beforeend", `
+        myTable.insertAdjacentHTML("beforeend", `
         <tr>
-            <td>${res[i].id}</td>
-            <td>${res[i].amount}</td>
-            <td>${res[i].type}</td>
-            <td><button id="delete">Delete</button></td>
-            <td><button id="update">Update</button></td>
+        <th>${res[i].id}</th>
+        <th>${res[i].amount}</th>
+        <th>${res[i].type}</th>
+        <td><button type="button" class="delete" data-id="${res[i].id}">Delete</button></td>
+        <td><button type="button" class="update" data-id="${res[i].id}">Update</button></td>
         </tr>
-        `);
+    `);}
+
+    
+    let myDelete = document.querySelectorAll(".delete");
+    let myUpdate = document.querySelectorAll(".update");
+    
+
+    myDelete.forEach(button => {
+        button.addEventListener("click", async (e) => {
+            e.preventDefault();
+            const id = e.target.getAttribute("data-id");
+            let eliminate = `https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla/${id}`;
+            
+            let config = {
+                method: "DELETE"
+            };
+            let res = await fetch(eliminate, config);
+            console.log(res);
+            location.reload();
+        }) 
+    });
+    let myReset = document.querySelector("#reset");
+    if (myReset) {
+        myReset.addEventListener("click", async (e) => {
+            // const confirmation = confirm("¿Estás seguro de que deseas eliminar todos los elementos?");
+            // if (confirmation) {
+            //     let config = {
+            //         method: "POST",
+            //         headers: { "Content-Type": "application/json" },
+            //         body: JSON.stringify(data)
+            //     };
+            //     for (let i = 1; i < res.length + 1; i++) {
+            //         let deleteResponse = await fetch(`https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla/${res[i].id}`, config);
+            //         console.log(deleteResponse);
+            //     }
+            //     location.reload();
+            // }
+            curl -X DELETE "https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla/";
+        });
     }
-})
+});
 
 myForm.addEventListener("submit", async (e) => {
     e.preventDefault();
@@ -86,15 +67,5 @@ myForm.addEventListener("submit", async (e) => {
     };
     let res = await (await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla", config)).json();
     console.log(res);
+    location.reload();
 })
-
-
-
-myDelete.addEventListener("click", async (e) => {
-    let config = {
-        method: "DELETE",
-        headers: { "Content-Type": "application/json" }
-    };
-    let res = await (await fetch("https://650ad5b7dfd73d1fab08fcc0.mockapi.io/tabla/tabla", config)).json();
-    console.log(res);
-})*/ 
